c6a89c3c60775106492b2410f8682dc6d9c53be9
Mimozer <mimoser@mail.ru>
2019-05-31 11:22:36 +0300

    Урок 1 задание 2.

---
+++ Lesson_1/Quest2/2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace Lesson_1
+{
+  class Class2
+  {
+    static void Main() //Here we create another example of simple program, with some math in it. You can count youre mass index.
+    {
+      double tal;
+      double mas;
+      Console.Write("Введите рост: ");
+      tal = Convert.ToDouble(Console.ReadLine());
+      Console.Write("Введите вес: ");
+      mas = Convert.ToDouble(Console.ReadLine());
+      double imt;
+      imt = mas / (tal * tal);
+      Console.Write("Ваш ИМТ равен " + imt);
+      Console.ReadKey();
+    }
+  }
+}
